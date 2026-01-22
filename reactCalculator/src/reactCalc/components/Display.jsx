3445beda5a6d82fd49c1ec16034812760e9f2749
Ryan Borel <rjborel@uno.edu>
2026-01-21 18:44:08 -0600

    reactCalc

---
+++ reactCalculator/src/reactCalc/components/Display.jsx
@@ -0,0 +1,13 @@
+import '../../index.css';
+
+const Display = ({outputBox, color = 'blue'}) =>{
+
+    return(
+        //output div that will house current and previous inputs
+        <div className="output">
+            
+
+
+        </div>
+    )
+}
