a9b289ef3f3aab78bdc4a7f15282d69df30190ab
Ryan Borel <rjborel@uno.edu>
2026-01-22 17:21:01 -0600

    reactCalc-update

--- reactCalculator/src/calc/components/Keypad.jsx
+++ reactCalculator/src/calc/components/Keypad.jsx
@@ -1,22 +1,49 @@
 import createDigits from "../util/createDigits";
 import createOperators from "../util/createOperators";
+import Display from "./Display";
+import { useState } from "react";
+import React from "react";
 
 const Keypad = () => {
-    let keyPad = keyOrder();   
+    let keyPadOrder = keyOrder();   
     return(
         <>
-            <button className="double-button" id="clearButton">AC</button>
-            <button className="delete-key" id="deleteButton">DEL</button>
-            {keyPad}
-            <button className="double-button" id="equalsButton">=</button>
+            <button className="double-button" id="clearButton" key={'allClear'}>AC</button>
+            <button className="delete-key" id="deleteButton" key={'delete'}>DEL</button>
+            {keyPadOrder}
+            <button className="double-button" id="equalsButton" key={'='}>=</button>
         </>
     )
 };
 export default Keypad;
 
+function addOnclick(buttonArr){
+    //let test = [<button className="test">test</button>]
+    //let result = [];
+
+    const addClick = (buttonArr) => {
+        return buttonArr.map((button) => {
+            return React.cloneElement(
+                button, {
+                    onClick: () => {
+                        console.log('working');
+
+                    }
+                }
+            ) 
+        })
+    }
+    
+    console.log(addClick(buttonArr));
+    return addClick(buttonArr);
+}
+
+
+
 
 function keyOrder(){
     let digits = createDigits();
+    //let digits = addOnclick(rawDigits);
     let operators = createOperators();
     let result = [];
 
@@ -28,14 +55,14 @@ function keyOrder(){
 
     digits.pop();
     digits.push(
-        <button className="number" id="bottom-left">.</button>
+        <button className="number" id="bottom-left" key={'.'}>.</button>
     )
     digits.push(
-        <button className="number">0</button>
+        <button className="number" key={'0'}>0</button>
     )
     
     result.push([...digits]);
-    console.log(result)
+    //console.log(result)
     
     return result;
 }
---
+++ reactCalculator/src/calc/hooks/useCalculator.jsx
@@ -0,0 +1,8 @@
+// import Display from "../components/Display";
+// import Keypad from "../components/Keypad";
+// import { useState } from "react";
+
+// function useCalculator(){
+//     const [currInput, setCurrInput] = useState('0');
+
+// };
