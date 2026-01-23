bcad618304eeee5e2d6bb1dfceb2f52380cd4db9
Ryan Borel <rjborel@uno.edu>
2026-01-22 23:01:13 -0600

    .

--- reactCalculator/src/calc/util/Assembler.jsx
+++ reactCalculator/src/calc/util/Assembler.jsx
@@ -1,11 +1,20 @@
 import Display from "../components/Display";
 import Keypad from "../components/Keypad";
+import { useState } from "react";
 
 const Assembler = () => {
+    const [currentInput, setCurrInput] = useState('0');
+    const [previousInput, setPrevInput] = useState('');
+
+    const updateDisplay = (newInput) => {
+        setCurrInput(newInput);
+        setPrevInput(currentInput);
+    }
+
     return(
         <div className="calculator-grid">
-            <Display/>           
-            <Keypad/>            
+            <Display currentInput={currentInput} previousInput={previousInput}/>           
+            <Keypad />            
         </div>
     );
 };
